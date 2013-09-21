42d50ee323a5c6bea2eb9e34067faed4f85d48f5
Adam <admgarrett@gmail.com>
2013-09-21 02:16:22 -0400

    fixed kernel version unknown

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Sep 21 00:27:57 EDT 2013"
+#define UTS_VERSION "#3 SMP PREEMPT Sat Sep 21 01:49:51 EDT 2013"
 #define LINUX_COMPILE_BY "adam"
 #define LINUX_COMPILE_HOST "adam-dev"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.31-DoubleshotKERNEL v1.0--gec57cda"
+#define UTS_RELEASE "3.0.31-DoubleshotKERNEL_v1.0--g00a3b47-dirty"
