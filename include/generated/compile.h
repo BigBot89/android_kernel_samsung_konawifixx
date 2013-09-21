00a3b477292c05a0d81470338d02d36fa9adf4b1
Adam <admgarrett@gmail.com>
2013-09-21 00:51:54 -0400

    removed config version

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Sep 21 00:27:57 EDT 2013"
+#define LINUX_COMPILE_BY "adam"
+#define LINUX_COMPILE_HOST "adam-dev"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.31-DoubleshotKERNEL v1.0--gec57cda"
